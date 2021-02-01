33fcbc2869cffee052e8059189712219d2f72d87
Max <maxnis@users.noreply.github.com>
2021-01-31 19:05:20 -0500

    Update info

--- cvApp.js
+++ cvApp.js
@@ -11,16 +11,17 @@ cvApp.controller("cvController", function ($scope) {
         summary: {
             title: "Software engineer with more than 10 years of experience in systems development.",
             description: "Technically sophisticated and highly accomplished .NET developer with a successful track record in systems analysis, design, and business process modelling. Skilled at providing specialized workflow solutions and technical support. Extensive experience of extending enterprise applications to support product profitability and customer retention. Passionate about producing reliable and maintainable code.",
-            short: "Software Development / Front-End & Back-End Development / Web & Mobile Applications / Windows and Web Services / Windows Server / SQL Server / IIS"
+            short: "Software Development / Front-End & Back-End Development / Web & Mobile Applications / Windows and Web Services / Unit tests / Windows Server / SQL Server / IIS"
         },
         techskills: [
-            { title: "Languages", icon: "file code", text: "C, C++, C#, VB.NET, T-SQL, LINQ, JavaScript, PowerShell, TypeScript." },
-            { title: "Tools", icon: "setting", text: "MS Visual Studio, VS-Code, JetBrains Rider." },
+            { title: "Languages", icon: "file code", text: "C, C++, C#, VB.NET, T-SQL, LINQ, JavaScript, PowerShell, TypeScript, Rust." },
+            { title: "Dev Tools", icon: "wrench", text: "MS Visual Studio, VS-Code, JetBrains Rider, JMeter." },
             { title: "Frameworks", icon: "code", text: ".NET 2.0 - 4.8, WCF, Nancy Fx, EF, NServiceBus."},
-            { title: "Web development", icon: "world", text: "ASP.NET, ASP.NET.MVC, Web API, HTML5, CSS, jQuery, jQueryMobile, Knockout JS, Angular, Bootstrap, Telerik Kendo UI." },
+            { title: "Web development", icon: "globe", text: "ASP.NET, ASP.NET.MVC, Web API, HTML5, CSS, jQuery, jQueryMobile, Knockout JS, Angular, Jasmine, Bootstrap, Telerik Kendo UI." },
+            { title: "Source control", icon: "git", text: "TFS, Git, GitHub." },
+            { title: "Unit Tests", icon: "chart bar", text: "MS Test, xUnit, Moq" },
             { title: "RDBMS", icon: "database", text: "MS SQL Server, SQLite." },
             { title: "NoSql", icon: "database", text: "MongoDB."},
-            { title: "Source control", icon: "fork", text: "TFS, Git, GitHub." },
             { title: "DevOps", icon: "server", text: "Jenkins, Sonar Qube, Azure DevOps, GitHub Actions."},
             { title: "Technology", icon: "cloud upload", text: "AWS, Azure." }
         ],
@@ -30,7 +31,14 @@ cvApp.controller("cvController", function ($scope) {
                 description: "Software solutions for contact centers",
                 url: "https://www.upstreamworks.com",
                 positions: [
-                    { title: "Senior Software Developer", period: "2016 - present" }
+                    { 
+                        title: "Senior Software Developer", period: "2016 - present",
+                        dolist: [
+                            "Code development and maintenance.",
+                            "Build, tests and deployment automation.",
+                            "Load tests maintenance, execution and analysis."
+                        ]
+                    }
                 ]
             },
             { 
@@ -42,12 +50,12 @@ cvApp.controller("cvController", function ($scope) {
                     { 
                         title: "Senior Software Developer", period: "2008 - 2016", 
                         dolist: [
-                        "Led hybrid mobile HTML5 project using AngularJS and Kendo UI and NancyFx to implement RESTful api on the back-end.", 
+                        "Led hybrid mobile HTML5 project using AngularJS and Kendo UI for the front-end and NancyFx to implement RESTful api as a back-end.", 
                         "Managed successful migration of web-hosted solutions to a new environment, ensuring that no data loss occurred in the process. Project involved both system and SQL Server version upgrade and required transferring of hundreds of websites and databases. Created automation tools to simplify the process of new releases and updates. Developed, automated and documented procedure of creating infrastructure for new customers including web sites and databases.",
                         "Headed development of multi-tier software containing cross-platform HTML5 mobile responsive web application having implemented offline functionality using websqldb and application cache.",
-                        "Designed multi-tenant architecture for data-driven web solutions across the company which reduced maintenance cost to a minimum.",
-                        "Initiated and completed an internal web based admin dashboard tool used by professional services and technical support. Tool provides features to register and configure customers in the system, test their working modules remotely replacing previous manual procedures - as a result reducing support time and simplifying problem identification.",
-                        "Collaborated with teams to improve the performance of systems including websites, web services, windows services, and desktop applications."
+                        "Designed multi-tenant architecture for data-driven hosted web products, which reduced maintenance costs.",
+                        "Initiated and completed an internal web based admin dashboard tool used by professional services and technical support. The tool provided features to register and configure customers in the hosting environment, test their working modules remotely replacing previous manual procedures thus reducing support time and simplifying problem identification.",
+                        "Collaborated with teams to improve performance of various systems including websites, web services, windows services, and desktop applications."
                     ]}
                 ] 
             },
@@ -60,15 +68,15 @@ cvApp.controller("cvController", function ($scope) {
                     { 
                         title: "Software Developer", period: "2006 - 2008", 
                         dolist: [
-                        "Maintained in-house device diagnostics and provisioning software tools.", 
-                        "Developed modules and features for the customer's web portal solution.",
-                        "Accomplished various SQL Server development projects as well as development and maintenance of different modules for real-time fleet management web portal."
+                            "Maintained in-house device diagnostics and provisioning software tools.",
+                            "Developed modules and features for the customer's web portal solution.",
+                            "Accomplished various SQL Server development projects as well as development and maintenance of different modules for real-time fleet management web portal."
                     ]}
                 ]
             },
             {
                 company: "Emka Group Inc.", address: "Ad Halom Ind. zone", country: "Israel", show: true,
-                description: "Emka Group is a direct mail provider and manufacturer of printing paper, envelopes, magnetic cards, etc.", 
+                description: "Emka Group is a direct mail provider and manufacturer of printing paper, envelopes, magnetic cards, etc.",
                 url: "http://www.emka.co.il/en",
                 positions:
                 [
